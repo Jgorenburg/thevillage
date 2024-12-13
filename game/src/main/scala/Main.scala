fe75b47feb67cb7863e22fab6fee8ccbbb8ded89
Jgorenburg <jacobgorenburg@gmail.com>
2024-12-13 15:15:16 -0500

    Change main to allow choice of game on commandline

--- game/src/main/scala/Main.scala
+++ game/src/main/scala/Main.scala
@@ -12,18 +12,30 @@ import com.badlogic.gdx.graphics.{GL20, OrthographicCamera}
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType
 import Snowedin.SnowedIn
+import DateNight.DateNight
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import scala.util.matching.Regex
 
-object DesktopLauncher extends App {
+object DesktopLauncher {
+  def main(args: Array[String]): Unit = {
+    println(args)
+    val DateNightPattern: Regex = """(?i)date.*""".r
+    val SnowedInPattern: Regex = """(?i)snow.*""".r
+    var game: ApplicationAdapter = new DateNight
+    if (args.nonEmpty) game = (args(0) match
+      case DateNightPattern() => new DateNight
+      case SnowedInPattern()  => new SnowedIn
+    )
 
-  val config = new Lwjgl3ApplicationConfiguration
-  config.setTitle("Snowed In")
+    val config = new Lwjgl3ApplicationConfiguration
+    config.setTitle("Snowed In")
 //   config.setWindowedMode(800, 600)
-  config.setMaximized(true)
-  config.setForegroundFPS(120)
+    config.setMaximized(true)
+    config.setForegroundFPS(120)
 
 //   config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode())
 
-  new Lwjgl3Application(new SnowedIn, config)
+    new Lwjgl3Application(game, config)
+  }
 }
